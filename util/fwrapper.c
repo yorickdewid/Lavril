62cd5052503df5bbca2ee403cae1c8c4b9951549
yorickdewid <yorick17@outlook.com>
2016-08-06 00:50:51 +0200

    Error handling for fcgi wrapper

--- util/fwrapper.c
+++ util/fwrapper.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
 #include <lavril.h>
 
 #ifdef SQUNICODE
@@ -49,6 +49,19 @@ const char *vars[MAX_VARS] = {
     "SERVER_SOFTWARE"
 };
 
+const char *error_body = "<html>"
+	"<head><title>%d %s</title></head>"
+	"<body bgcolor=\"white\">"
+	"<h1>%d %s</h1>"
+	"%s"
+	"<hr>lavril"
+	"</body>"
+	"</html>\r\n";
+
+void print_error(int code, const char *status, const char *message) {
+	scprintf(error_body, code, status, code, status, message ? message : "");
+}
+
 void print_func(HSQUIRRELVM v, const SQChar *s, ...) {
 	va_list vl;
 	va_start(vl, s);
@@ -63,69 +76,49 @@ void error_func(HSQUIRRELVM v, const SQChar *s, ...) {
 	va_end(vl);
 }
 
-void call_test(HSQUIRRELVM v, int n, float f, const SQChar *s) {
-	/* Save the stack size before the call */
-	SQInteger top = lv_gettop(v);
-
-	/* Push the global table */
-	lv_pushroottable(v);
-	lv_pushstring(v, _SC("test"), -1);
-
-	/* Get the routine 'test' from the global table */
-	if (LV_SUCCEEDED(lv_get(v, -2))) {
-		lv_pushroottable(v);
-		lv_pushinteger(v, n);
-		lv_pushfloat(v, f);
-		lv_pushstring(v, s, -1);
-		lv_call(v, 4, SQFalse, SQTrue);
-	}
-
-	/* Restore the original stack size */
-	lv_settop(v, top);
-}
-
 int main(int argc, char *argv[]) {
-	HSQUIRRELVM v;
-
-	/* Creates a VM with initial stack size 1024 */
-	v = lv_open(1024);
-
-	{
-		/* Push the global table to store std */
-		lv_pushroottable(v);
-
-		/* Load modules */
-		init_module(math, v);
-
-		/* Pop the root table */
-		lv_pop(v, 1);
-	}
-
-	/* Registers the default error handlers */
-	lv_registererrorhandlers(v);
-
-	/* Sets the print function */
+	HSQUIRRELVM v = lv_open(1024);
 	lv_setprintfunc(v, print_func, error_func);
 
-	/* Push the root table(were the globals of the script will be stored) */
+	/* Push the roottable */
 	lv_pushroottable(v);
 
-	/* Print syntax errors if any */
+	/* Load modules */
+	init_module(blob, v);
+	init_module(io, v);
+	init_module(string, v);
+	init_module(system, v);
+	init_module(math, v);
+	init_module(crypto, v);
 
+	lv_registererrorhandlers(v);
+
+	/* Accept requests */
     while (FCGI_Accept() >= 0) {
 		char *script = getenv("SCRIPT_NAME");
 		if (!script) {
-			printf("Status: 500\r\nContent-type: text/plain\r\n\r\n");
-			puts("no script name provided");
+			scprintf("Status: 500\r\nContent-type: text/html\r\n\r\n");
+			print_error(500, "Internal Server Error", "No script name provided");
 			continue;
 		}
 
 		if (script[0] == '/')
 			script++;
 
-		printf("Content-type: text/plain\r\n\r\n");
+		if (LV_FAILED(access(script, F_OK))) {
+			scprintf("Status: 404\r\nContent-type: text/html\r\n\r\n");
+			print_error(404, "Not Found", NULL);
+			continue;
+		}
+
+		scprintf("Content-type: text/html\r\n\r\n");
 		if (LV_FAILED(lv_execfile(v, script, SQFalse, SQTrue)))  {
-			puts("an unknown error occurred");
+			const SQChar *err;
+			lv_getlasterror(v);
+			if (LV_SUCCEEDED(lv_getstring(v, -1, &err))) {
+				scprintf(_SC("<br /><b>An error occurred:</b> <pre>[%s]</pre>\r\n"), err);
+				lv_reseterror(v);
+			}
 		}
 	}
 
